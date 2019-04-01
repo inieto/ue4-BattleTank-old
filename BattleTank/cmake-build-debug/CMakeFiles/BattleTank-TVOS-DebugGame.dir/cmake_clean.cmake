ed5c0cd91e4a6dafe260ae2bb8398e51f0b2fe29
Ignacio Nieto <nietofarias@gmail.com>
2019-04-01 01:57:42 -0300

    Section 5 - Lecture 120 - Terrain creation
    And CLion project generation

---
+++ BattleTank/cmake-build-debug/CMakeFiles/BattleTank-TVOS-DebugGame.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/BattleTank-TVOS-DebugGame"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/BattleTank-TVOS-DebugGame.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ BattleTank/cmake-build-debug/CMakeFiles/BattleTank-TVOS.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/BattleTank-TVOS"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/BattleTank-TVOS.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
